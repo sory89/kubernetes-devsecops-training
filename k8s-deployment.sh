bea48bd7263259fa99bf2a75e8b77dfc0763a77b
sory89 <sordiallo@gmail.com>
2025-04-16 12:25:50 +0200

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,13 +3,13 @@
 #k8s-deployment.sh
 
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
-kubectl -n default get deployment ${deploymentName} > /dev/null
+sudo -u vagrant kubectl -n default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
     echo "deployment ${deploymentName} doesnt exist"
-    kubectl -n default apply -f k8s_deployment_service.yaml
+   sudo -u vagrant kubectl -n default apply -f k8s_deployment_service.yaml
 else
     echo "deployment ${deploymentName} exist"
     echo "image name - ${imageName}"
-    kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+    sudo -u vagrant kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
 fi
